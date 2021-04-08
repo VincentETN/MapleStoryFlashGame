03ae7848d5ec293909ea1d26414b0227db8c5856
VincentETN <vincent8947@gmail.com>
2021-04-08 15:29:00 +0800

    platform

---
+++ MSGame/Source/Platform.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace game_framework {
+	//以一條線表示平台，除了斜坡，y1==y2
+
+	class Platform {
+	public:
+		Platform();
+		int getX1();	//左端
+		int getY1();
+		int getX2();	//右端
+		int getY2();
+		void setXY(int nx1, int ny1, int nx2, int ny2);
+		int movingSpeed();	//tell player step
+	protected:
+		int x1, y1, x2, y2;
+	};
+}
