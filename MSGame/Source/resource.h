9ddd19ed0d61c427c5a61479bac960bac05e12c4
陳彥任 <alan30412@gmail.com>
2021-04-23 10:44:26 +0800

    stage2bg modified

--- MSGame/Source/resource.h
+++ MSGame/Source/resource.h
@@ -83,8 +83,9 @@
 #define IDB_C_AR4                       211
 #define IDB_C_AR5                       212
 #define IDB_C_AR6                       213
-#define IDB_BITMAP6                     214
 #define IDB_C_AR7                       214
+#define IDB_BITMAP3                     216
+#define IDB_STAGE2_BG                   216
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -97,7 +98,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        215
+#define _APS_NEXT_RESOURCE_VALUE        217
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
