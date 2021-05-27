1b14b51fee350f40929ddad14160b5989faf29e1
VincentETN <vincent8947@gmail.com>
2021-05-27 17:42:58 +0800

    nothing change

--- MSGame/Source/CPlayer.h
+++ MSGame/Source/CPlayer.h
@@ -2,25 +2,23 @@
 #include "Ladder.h"
 
 namespace game_framework {
-	/////////////////////////////////////////////////////////////////////////////
-	// 這個class提供可以用鍵盤或滑鼠控制的擦子
-	// 看懂就可以改寫成自己的程式了
-	/////////////////////////////////////////////////////////////////////////////
-
 	class CPlayer
 	{
 	public:
 		CPlayer();
+		/*************************************************/
 		int  GetX1();					// 玩家左上角 x 座標
 		int  GetY1();					// 玩家左上角 y 座標
 		int  GetX2();					// 玩家右下角 x 座標
 		int  GetY2();					// 玩家右下角 y 座標
 		int  GetMidX();					// 玩家中心 x 座標
 		int  GetMidY();					// 玩家中心 y 座標
+		/*************************************************/
 		void Initialize();				// 設定初始值
 		void LoadBitmap();				// 載入圖形
 		void OnMove();					// 移動擦子
 		void OnShow();					// 將擦子圖形貼到畫面
+
 		void SetMovingDown(bool flag);	// 設定是否正在往下移動
 		void SetMovingLeft(bool flag);	// 設定是否正在往左移動
 		void SetMovingRight(bool flag); // 設定是否正在往右移動
@@ -33,6 +31,7 @@ namespace game_framework {
 		void SetGetHurt(bool flag);
 		void SetXY(int nx, int ny);		// 設定擦子左上角座標
 		void SetIsClimbing(bool flag);
+
 		bool IsInTheAir();
 		bool IsOnTheGround();
 		bool Attacking();
